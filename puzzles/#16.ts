c0aa2e197f759faea1ed95873f353594769f51c0
Sandro Baccega <sandro.baccega@wirecube.at>
2023-12-16 09:18:00 +0100

    ✨ #16

---
+++ puzzles/#16.ts
@@ -0,0 +1,17 @@
+type FindSantaArray<T extends any[], Acc extends any[] = []> = T extends [
+  infer Char extends any,
+  ...infer Rest extends any[]
+]
+  ? Char extends "🎅🏼"
+    ? Acc["length"]
+    : FindSantaArray<Rest, [...Acc, Char]>
+  : never;
+
+type FindSantaMatrix<T extends any[][], Acc extends any[] = []> = T extends [
+  infer Array extends any[],
+  ...infer Rest extends any[][]
+]
+  ? FindSantaArray<Array> extends never
+    ? FindSanta<Rest, [...Acc, Array]>
+    : [Acc["length"], FindSantaArray<Array>]
+  : never;
